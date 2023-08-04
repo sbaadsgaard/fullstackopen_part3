9783b3950af55fc249472f6d7a816ed8911b5e3d
sbaadsgaard <sbaadsgaard@live.dk>
2023-08-04 18:44:25 +0200

    finish 3.8

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express()
 const PORT = 3001
 
 app.use(express.json())
-app.use(morgan("tiny"))
+morgan.token("body", (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+
+
+
 let persons = [
     { 
       "id": 1,
