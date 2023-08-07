477f3ff081d1aaa64c1128f782a82495049c151c
sbaadsgaard <sbaadsgaard@live.dk>
2023-08-07 15:55:48 +0200

    finish 3.12

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+if (process.argv.length != 3 && process.argv.length != 5) {
+    console.log("Usage: node mongo.js  <passworld> [<name> <number>](optional)")
+    process.exit(1)
+}
+
+mongoose.set("strictQuery", false)
+const pw = process.argv[2]
+const uri = `mongodb+srv://baaden22:${pw}@cluster0.fd64drg.mongodb.net/noteApp?retryWrites=true&w=majority`
+mongoose.connect(uri)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv.length == 3) {
+    Person.find({})
+        .then(people => {
+            console.log("Phonebook:")
+            people.forEach(p =>
+                console.log(`${p.name} ${p.number}`))
+            mongoose.connection.close()
+        })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+        name,
+        number
+    })
+    person.save()
+        .then(res => {
+            console.log(`Added ${res.name} number ${res.number} to phonebook`)
+            mongoose.connection.close()
+        })
+}
